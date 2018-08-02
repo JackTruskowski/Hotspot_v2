357d120bd8ac8550df77933f607a3c363195f726
Jack Truskowski <jtruskow@gmail.com>
2018-08-02 13:35:48 -0500

    implement user authentication using cookie

--- hotflask/hotspot.py
+++ hotflask/hotspot.py
@@ -2,60 +2,29 @@ import os
 import sqlite3
 from flask import Flask
 from flask import render_template
-from flask import request
+from flask import request, redirect, url_for, session, flash
 from flask_login import current_user
 from flask_login import LoginManager
+from functools import wraps
 import db
 
 app = Flask(__name__)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+app.secret_key = "something unique and secret"
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 DATABASE = os.path.join(PROJECT_ROOT, 'instance', 'hotflask.sqlite')
-USERNAME = "Default"
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
-
-@app.route('/login', methods=['POST', 'GET'])
-def loginPage():
-
-    if request.method == 'POST':
-
-        if(request.form.get("newaccount") != None):
-            user = db.add_user(request.form.get("username"), request.form.get("password"))
-        else:
-            user = db.get_user(request.form.get("username"), request.form.get("password"))
-            
-        if not user:
-            return render_template('login.html', error="Failed to authenticate.")
-        else:
-            USERNAME = user[0]
-            return render_template('index.html', data=db.get_default_data())
-
-    return render_template('login.html', error=None)
-
-@app.route('/user', methods=['GET', 'POST'])
-def userPage():
-    
-    conn = sqlite3.connect(DATABASE)
-    cur = conn.cursor()
-    
-    if request.method == 'POST':
-        cur.execute("INSERT INTO likes VALUES (?, ?)", (request.form['id'], USERNAME))
-        conn.commit()
-
-    cur.execute("SELECT x.rname, x.address, x.zipcode, city.cname, x.rating, x.price_range FROM (SELECT *  FROM restaurant r INNER JOIN likes ON r.rest_id=likes.restaurant_id)x INNER JOIN city ON x.zipcode=city.zipcode LIMIT 100;")
-    
-    data = cur.fetchall()
-    return render_template('user.html', data=data, user=USERNAME)
-
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('loginPage'))
 
 @app.route('/', methods=['GET', 'POST'])
 def defaultPage():
+
+    if not 'user' in session:
+        return redirect(url_for('loginPage'))
 
     # User is filtering
     if request.method == 'POST':
@@ -87,6 +56,44 @@ def defaultPage():
     data = db.get_default_data()
 
     return render_template('index.html', data=data)
+
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def loginPage():
+    if request.method == 'POST':
+
+        if(request.form.get("newaccount") != None):
+            user = db.add_user(request.form.get("username"), request.form.get("password"))
+        else:
+            user = db.get_user(request.form.get("username"), request.form.get("password"))
+            
+        if not user:
+            return render_template('login.html', error="Failed to authenticate.")
+        else:
+            session['logged_in'] = True
+            session['user'] = request.form.get("username")
+            return redirect(url_for('defaultPage'))
+        #return render_template('index.html', data=db.get_default_data())
+
+    return render_template('login.html', error=None)
+
+@app.route('/user', methods=['GET', 'POST'])
+def userPage():
+    
+    conn = sqlite3.connect(DATABASE)
+    cur = conn.cursor()
+    
+    if request.method == 'POST':
+        cur.execute("INSERT INTO likes VALUES (?, ?)", (request.form['id'], session['username']))
+        conn.commit()
+
+    cur.execute("SELECT x.rname, x.address, x.zipcode, city.cname, x.rating, x.price_range FROM (SELECT *  FROM restaurant r INNER JOIN likes ON r.rest_id=likes.restaurant_id)x INNER JOIN city ON x.zipcode=city.zipcode LIMIT 100;")
+    
+    data = cur.fetchall()
+    return render_template('user.html', data=data, user=session['username'])
+
+
 
 @app.route('/search', methods=['GET', 'POST'])
 def searchPage():
