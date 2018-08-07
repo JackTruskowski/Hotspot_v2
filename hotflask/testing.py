93cafb3ed1e0be24f72e2cd0d98e0ddd01c57d82
Jack Truskowski <jtruskow@gmail.com>
2018-08-06 21:13:00 -0500

    update script

--- hotflask/testing.py
+++ hotflask/testing.py
@@ -2,6 +2,8 @@ import db
 import os
 import time
 import random
+import sqlite3
+import string
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 DATABASE = os.path.join(PROJECT_ROOT, 'instance', 'hotflask.sqlite')
@@ -10,20 +12,25 @@ DATABASE = os.path.join(PROJECT_ROOT, 'instance', 'hotflask.sqlite')
 print("Getting random restaurants by ID...")
 total_time = 0.0
 queries_run = 0
+longest_time = 0.0
 for x in range(1000):
     start = time.time()
     result = db.get_restaurant(str(random.randint(0, 25000)))
     end = time.time()
+    if end-start > longest_time:
+        longest_time = end-start
     total_time += (end-start)
     queries_run += 1
 
 print("Total queries run: " + str(queries_run))
-print("Total time: " + str(total_time) + " sec")
-print("Average time per query: " + str(total_time/queries_run) + " sec\n")
+print("Total time: " + "{0:0.4f}".format(total_time) + " sec")
+print("Average time per query: " + "{0:0.4f}".format(total_time/queries_run) + " sec")
+print("Longest query time: " + "{0:0.4f}".format(longest_time) + " sec\n")
 
 #run some random queries
 print("Executing random filterings...")
 total_time = 0.0
+longest_time = 0.0
 queries_run = 0
 for x in range(1000):
     
@@ -44,11 +51,56 @@ for x in range(1000):
     result = db.do_query(lower_bound, upper_bound, price_range, rand_rest[9])
     end = time.time()
 
+    if end-start > longest_time:
+        longest_time = end-start
+
     total_time += (end-start)
     queries_run += 1
 
 print("Total queries run: " + str(queries_run))
-print("Total time: " + str(total_time) + " sec")
-print("Average time per query: " + str(total_time/queries_run) + " sec\n")
-    
+print("Total time: " + "{0:0.4f}".format(total_time) + " sec")
+print("Longest time: " + "{0:0.4f}".format(longest_time) + " sec")
+print("Average time per query: " + "{0:0.4f}".format(total_time/queries_run) + " sec\n")
 
+
+print("Adding many random users...")
+total_time = 0.0
+longest_time = 0.0
+queries_run = 0
+usernames = []
+for x in range(1000):
+    conn = sqlite3.connect(DATABASE)
+    cur = conn.cursor()
+    rand_name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+    usernames.append(rand_name)
+    start = time.time()
+    cur.execute("INSERT INTO user VALUES (?, ?)", (rand_name, "supersecurepassword"))
+    conn.commit()
+    end = time.time()
+    if end-start > longest_time:
+        longest_time = end-start
+    total_time += (end-start)
+    queries_run += 1
+print("Total queries run: " + str(queries_run))
+print("Total time: " + "{0:0.4f}".format(total_time) + " sec")
+print("Average time per query: " + "{0:0.4f}".format(total_time/queries_run) + " sec")
+print("Longest query time: " + "{0:0.4f}".format(longest_time) + " sec\n")
+
+
+print("Deleting the random users...")
+total_time = 0.0
+longest_time = 0.0
+queries_run = 0
+for username in usernames:
+    start = time.time()
+    cur.execute("DELETE FROM user WHERE username like \"" + username + "\" ;")
+    conn.commit()
+    end = time.time()
+    if end-start > longest_time:
+        longest_time = end-start
+    total_time += (end-start)
+    queries_run += 1
+print("Total queries run: " + str(queries_run))
+print("Total time: " + "{0:0.4f}".format(total_time) + " sec")
+print("Average time per query: " + "{0:0.4f}".format(total_time/queries_run) + " sec")
+print("Longest query time: " + "{0:0.4f}".format(longest_time) + " sec\n")
